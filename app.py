af4a171d1d4937964bbea7e7b8fb9822f151524a
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-05-20 12:56:30 +0530

    new

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
+from email import message
 from flask import Flask, render_template, request
 from sqlalchemy import true
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        customer = request.form['customer']
+        dealer = request.form['dealer']
+        rating = request.form['rating']
+        comments = request.form['comments']
+
+        if customer == '' or dealer == '':
+            return render_template('index.html', message='Please enter required fields!')
+
+        return render_template('success.html')
 
 if __name__ == '__main__':
     app.debug = True
