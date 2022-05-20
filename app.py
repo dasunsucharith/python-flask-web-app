d3cfdedafe7702fd85b48691a692af122ebc0520
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-05-20 13:28:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1993@localhost/elife'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -23,5 +36,4 @@ def submit():
         return render_template('success.html')
 
 if __name__ == '__main__':
-    app.debug = True
     app.run()
