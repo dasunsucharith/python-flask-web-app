1c9b367d11343fe69e75b7968cae1c5f672a1821
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-05-21 21:01:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from email import message
+from enum import unique
 from flask import Flask, render_template, request
 from sqlalchemy import true
 from flask_sqlalchemy import SQLAlchemy
@@ -18,6 +19,20 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200), unique=True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self, customer, dealer, rating, comments):
+        self.customer = customer
+        self.dealer = dealer 
+        self.rating = rating 
+        self.comments = comments 
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -32,8 +47,12 @@ def submit():
 
         if customer == '' or dealer == '':
             return render_template('index.html', message='Please enter required fields!')
-
-        return render_template('success.html')
+        if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
+            data = Feedback(customer, dealer, rating, comments)
+            db.session.add(data)
+            db.session.commit()
+            return render_template('success.html')
+        return render_template('index.html', message='You have already submitted feedback!')
 
 if __name__ == '__main__':
     app.run()
